9e0e7c2476076c07c7b61b557dcef0d564803d2e
Flying Kitlets <aaronjohnstanton@gmail.com>
2019-10-18 15:56:41 +0100

    updated about controller

--- controllers/about.js
+++ controllers/about.js
@@ -1,4 +1,4 @@
 exports.aboutApi = (req, res, next) => {
   const aboutObj = require("../endpoints.json");
-  res.status(200).send({ about: JSON.stringify(aboutObj) });
+  res.status(200).send(JSON.stringify(aboutObj));
 };
